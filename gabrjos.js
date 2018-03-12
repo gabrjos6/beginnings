80791cf553ea1734ab9f45899a714cb76f306462
gabrjos6 <37160689+gabrjos6@users.noreply.github.com>
2018-03-12 15:02:36 -0700

    Update gabrjos.js

--- gabrjos.js
+++ gabrjos.js
@@ -1,4 +1,13 @@
 var t = prompt("What precentage did you get (only list number)"); 
-if(t > 92 && t < 100) {
-alert(your grade is an A);
+if(t === "100") {
+alert("You got an A+!");
+}
+if(t > "92" && t < "100") {
+alert("your grade is an A");
+}
+if(t === "92" || t < "92" && t > "89") {
+alert("You got an A-");
+}
+if(t > "88" && t < "90") {
+alert("You got a B+");
 }
