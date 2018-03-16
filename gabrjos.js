124a6cf86c863188af67e6b53fd38d32b7c5f9bf
Levi Thorne <35429528+PixelXII@users.noreply.github.com>
2018-03-15 19:45:01 -0700

    Added comments
    I added a comment directing YOU to include 100. Right now, upon entering 100, the prompt boxes close and nothing happens.

--- gabrjos.js
+++ gabrjos.js
@@ -1,5 +1,5 @@
 var t = prompt("What precentage did you get (only list number (ie 100)"); 
-if(t > 100 && t < 201) {
+if(t > 100 && t < 201) { //Ya screwed up here. There is no specification for what happens when the input IS ACTUALLY 100. Change the parameters to t > 99, so it INCLUDES 100.
   alert("You got an A+!");
 }
 if(t > 92 && t < 100) {
