1ddfb33e22b19a93f7ec03e713ce483ea92a3045
altD <35429528+PixelXII@users.noreply.github.com>
2018-05-03 19:48:22 -0700

    Update Rock.js

--- Rock.js
+++ Rock.js
@@ -1,4 +1,9 @@
 function random() {
-  var x = Math.floor(Math.random()*10)
-  alert(x);
+  var x = Math.floor(Math.random()*10);
+  var conf = confirm('setInterval?');
+  if(conf == false) {
+    alert(x);
+  } else if (conf == true) {
+    setInterval(function() {document.getElementsByTagName('h1').innerHTML = x;}, 2000);
+  }
 }
