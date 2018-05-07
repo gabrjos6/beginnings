5a1fd24591d351044af2e1ed69ed78a72dc247ec
gabrjos6 <37160689+gabrjos6@users.noreply.github.com>
2018-05-07 11:52:08 -0700

    Update Rock.js

--- Rock.js
+++ Rock.js
@@ -1,7 +1,12 @@
 function random() {
-  var x = Math.floor(Math.random()*10);
-  var conf = confirm('setInterval?');
-  if(conf == false) {
-    alert(x);
+  var x = Math.floor(Math.random()*10)
+    alert(x)
+  if(x < 3 || x = 3) {
+    x = "r"
   }
+  if(x > 3 && x < 7) {
+    x = "p"
+  }
+  if(x >
+  var y = prompt("Rock(1), Paper(2), or sissors(3)")
 }
