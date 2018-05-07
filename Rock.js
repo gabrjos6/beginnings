e2ca61eb4d711ff9bf61f852afd33ea8a8ab6f04
gabrjos6 <37160689+gabrjos6@users.noreply.github.com>
2018-05-07 11:59:51 -0700

    Update Rock.js

--- Rock.js
+++ Rock.js
@@ -2,11 +2,19 @@ function random() {
   var x = Math.floor(Math.random()*10)
     alert(x)
   if(x < 3 || x = 3) {
-    x = "r"
+    x = "1"
   }
   if(x > 3 && x < 7) {
-    x = "p"
+    x = "2"
   }
-  if(x >
+  if(x > 6 && x < 10) {
+    x = "3"
+  }
+  if(x === 10) {
+    x = "4"
   var y = prompt("Rock(1), Paper(2), or sissors(3)")
+if(y === "1" && x === "1") {
+  alert("You tied. You both picked rock.");
 }
+    if(y === "" && x === ) {
+       
