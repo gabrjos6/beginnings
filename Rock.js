3e56aa9167734f2337c0bc382814715dfe6e296d
gabrjos6 <37160689+gabrjos6@users.noreply.github.com>
2018-05-08 12:28:39 -0700

    Update Rock.js

--- Rock.js
+++ Rock.js
@@ -12,6 +12,7 @@ function random() {
   }
   if(x === 10) {
     x = "4"
+  }
   var y = prompt("Rock(1), Paper(2), or scissors(3)")
 if(y === "1" && x === "1") {
   alert("You tied. You both picked rock.");
@@ -23,10 +24,27 @@ if(y === "1" && x === "1") {
       alert("You tied. You both picked scissors");
     }
     if(x === 4) { 
-      alert("You lost. The computer picked nuclear bomb.")
+      alert("You lost. The computer picked nuclear bomb.");
     }
     if(y === 1 && x === 2) {
-      alert("You lost. You picked rock and the computer picked scissors.")
+      alert("You lost. You picked rock and the computer picked paper.");
     }
-    if(y === 1 && x === 3) { 
-      alert("You win
+  if(y === 1 && x === 3) { 
+      alert("You win. You picked rock and the computer picked scissors");
+  }
+  if(y === 2 && x === 1) {
+    alert("You win. You picked paper and the computer picked rock.");
+  }
+  if(y === 2 && x === 3) {
+    alert("You lose. You picked paper and the computer picked scissors");
+  }
+  if(y === 3 && x === 1) {
+    alert("You lose. You picked scissors and the computer picked rock.");
+  }
+  if(y === 3 && x === 2) {
+    alert("You win. You picked scissors and the computer picked paper.");
+  }
+  else {
+    alert("sorry, we don't understand your input") 
+  }
+}
