d28c908e4a43514cfd5ce834414e525af8dd3736
gabrjos6 <37160689+gabrjos6@users.noreply.github.com>
2018-05-09 12:22:41 -0700

    Update Rock.js

--- Rock.js
+++ Rock.js
@@ -1,46 +1,46 @@
 function random() {
   var x = Math.floor(Math.random()*10)
-  if(x < 3 || x = 3) {
-    x = "1"
+  if(x < 3 || x === 3) {
+    z = "1"
   }
   if(x > 3 && x < 7) {
-    x = "2"
+    z = "2"
   }
   if(x > 6 && x < 10) {
-    x = "3"
+    z = "3"
   }
   if(x === 10) {
-    x = "4"
+    z = "4"
   }
   var y = prompt("Rock(1), Paper(2), or scissors(3)")
 if(y === "1" && x === "1") {
   alert("You tied. You both picked rock.");
 }
-    if(y === 2 && x === 2) {
+    if(y === 2 && z === 2) {
        alert("You tied. You both picked paper");
     }
-    if(y === 3 && x === 3) { 
+    if(y === 3 && z === 3) { 
       alert("You tied. You both picked scissors");
     }
-    if(x === 4) { 
+    if(x === 4 && z === 4) { 
       alert("You lost. The computer picked nuclear bomb.");
     }
-    if(y === 1 && x === 2) {
+    if(y === 1 && z === 2) {
       alert("You lost. You picked rock and the computer picked paper.");
     }
-  if(y === 1 && x === 3) { 
+  if(y === 1 && z === 3) { 
       alert("You win. You picked rock and the computer picked scissors");
   }
-  if(y === 2 && x === 1) {
+  if(y === 2 && z === 1) {
     alert("You win. You picked paper and the computer picked rock.");
   }
-  if(y === 2 && x === 3) {
+  if(y === 2 && z === 3) {
     alert("You lose. You picked paper and the computer picked scissors");
   }
-  if(y === 3 && x === 1) {
+  if(y === 3 && z === 1) {
     alert("You lose. You picked scissors and the computer picked rock.");
   }
-  if(y === 3 && x === 2) {
+  if(y === 3 && z === 2) {
     alert("You win. You picked scissors and the computer picked paper.");
   }
   else {
