1c1cf036064175b4cc7153ad8957fa1a5902fbde
gabrjos6 <37160689+gabrjos6@users.noreply.github.com>
2018-06-13 11:28:39 -0700

    Update Lottery.js

--- Lottery.js
+++ Lottery.js
@@ -1,11 +1,11 @@
 function Lottery() {
-  x = x + 2
     var t = Math.round(Math.random()*120000001) 
     if(t ==="25924331") {
         alert("YOU WIN!!!!! !!!!! !!!!!"); 
     }
     if(t < 25924331 || t > 25924331) {
         alert("You lose :( "); 
+    x = x + 2
     }
 }
 function MONEY() {
