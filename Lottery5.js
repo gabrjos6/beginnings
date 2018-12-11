d86866f904e4bfc1c3f9c52da7fa529609167e55
gabrjos6 <37160689+gabrjos6@users.noreply.github.com>
2018-12-11 09:51:30 -0800

    Update Lottery5.js

--- Lottery5.js
+++ Lottery5.js
@@ -1,7 +1,20 @@
 function lottery5() {
-   function Lottery(); 
+  var t = prompt("Number of tickets")
+  if t = 1 {
+  function Lottery(); 
+  }
+   if t > 1 {
+      function Lottery();
+   }
+   if t > 2 {
    function Lottery();
+   }
+   if t > 3 {
    function Lottery();
-   function Lottery();
+   }
+   if t > 4 {
    function Lottery();
 }
+   if t > 5 {
+      function Lottery();
+   }
