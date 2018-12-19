ccd3b0fc7d81a1bd202f5d610e9e44e0863162c0
altD <35429528+PixelXII@users.noreply.github.com>
2018-12-19 13:22:52 -0800

    Update Lottery5.js

--- Lottery5.js
+++ Lottery5.js
@@ -1,63 +1,7 @@
 function lottery5() {
    var i = prompt("How many tickets do you want? (up to twenty)");
-  if(i > 0) {
-  Lottery(); 
+   
+  for(var a = 0; a < i; a++) {
+     Lottery5();
   }
-   if(i > 1) {
-  Lottery();
-   }
-   if(i > 2) {
-  Lottery();
-   }
-   if(i > 3) {
-  Lottery();
-   }
-   if(i > 4) {
-      Lottery();
-}
-   if(i > 5) {
-      Lottery();
-   }
-   if(i > 6) {
-      Lottery();
-   }
-   if(i > 7) {
-      Lottery();
-   }
-   if(i > 8) {
-      Lottery();
-   }
-   if(i > 9) {
-      Lottery();
-   }
-    if(i > 10) {
-      Lottery();
-   }
-    if(i > 11) {
-      Lottery();
-   }
-     if(i > 12) {
-      Lottery();
-   }
-      if(i > 13) {
-      Lottery();
-   }
-      if(i > 14) {
-      Lottery();
-   }
-       if(i > 15) {
-      Lottery();
-   }
-       if(i > 16) {
-      Lottery();
-   }
-      if(i > 17) {
-      Lottery();
-   }
-      if(i > 18) {
-      Lottery();
-   }
-      if(i > 19) {
-      Lottery();
-   }
 }
