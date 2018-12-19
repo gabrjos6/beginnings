d58a94f2e480b1708a9345861da713b5dccba975
gabrjos6 <37160689+gabrjos6@users.noreply.github.com>
2018-12-19 13:23:08 -0800

    Update Lottery5.js

--- Lottery5.js
+++ Lottery5.js
@@ -1,5 +1,5 @@
 function lottery5() {
-   var i = prompt("How many tickets do you want? (up to twenty)");
+   var i = prompt("How many tickets do you want?");
    
   for(var a = 0; a < i; a++) {
      Lottery5();
