1630407755751177af269e65ad914c385fe58409
altD <35429528+PixelXII@users.noreply.github.com>
2018-12-19 13:31:21 -0800

    Update element.js

--- element.js
+++ element.js
@@ -1,1 +1,8 @@
-
+var elementCreate = () => {
+  var getId = v => {
+    return document.getElementById(v)
+  }
+  var p = document.createElement("span")
+  p.id = 'createdElement';
+  p.innerHTML = 'boi u should learn how to deal with the DOM in javascript its worth it thats how i made all me text games'
+}
