7474ddab1ce3673e1cbb09dc10651e3527e2ebb6
altD <35429528+PixelXII@users.noreply.github.com>
2018-12-19 13:26:31 -0800

    Update Lottery5.js

--- Lottery5.js
+++ Lottery5.js
@@ -1,7 +1,13 @@
+let b = false;
 function lottery5() {
-   var i = prompt("How many tickets do you want?");
+   if(!b) {
+      var i = prompt("How many tickets do you want?");
+   } else {
+      alert(`You bought ${i} tickets.`)
+   }
    
   for(var a = 0; a < i; a++) {
      lottery5();
+     b = true;
   }
 }
